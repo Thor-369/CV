f12df06c30e22510202697847c2c9db3442fc0c2
juanpablo369 <52577050+juanpablo369@users.noreply.github.com>
2020-03-19 17:42:30 -0500

    automatize++

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,17 @@ var roots = new rootsController();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Datos actualizados del Covid-19 Ecuador' });
+  res.render('index', { title: 'Datos actualizados del Covid-19 Ecuador',
+  	casos:"260", 
+  	muertos:"4",
+  	recuperados:"3",
+  	descartados:"412",
+  	sospechosos:"481",
+  	pruebas:"1153",
+  	aislamiento:"236",
+  	estables:"12",
+  	reservado:"5",
+  	 });
 });
 
 router.get('/datos',roots.datos);
