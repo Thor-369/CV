05abc550f6d6b8648979a3dfdd798bdc5ca027c8
juanpablo369 <52577050+juanpablo369@users.noreply.github.com>
2020-03-31 17:30:55 -0500

    tarde 31/03/20

--- routes/index.js
+++ routes/index.js
@@ -2,46 +2,30 @@ var express = require('express');
 var router = express.Router();
 
 
-const webpush = require("web-push");
-webpush.setVapidDetails(
-	"mailto:thor.devman@gmail.com",
-	process.env.Public_V_Key,
-	process.env.Private_V_Key
-	);
+//web push
+var webpush = require=("/webpush");
 
- let pushSubscription;
-router.post("/subscription", (req, res)=> {
-	pushSubscription = req.body;
-	res.status(200).json();
 
-	const payload = JSON.stringfy({
-		title: "Notificación de Pablo",
-		message: "Hello Nigga"
-	})
-	webpush.sendNotification(pushSubscription, payload)
-});
 
-var rootsController = require('../controller/Roots');
-var roots = new rootsController();
-var cases= "2240";
+var cases= "2302";
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: cases+' casos de Covid-19 Ecuador',
   	
   	casos:cases, 
-  	muertos:"75",
-  	recuperados:"54",
-  	descartados:"2485",
-  	sospechosos:"3257",
-  	pruebas:"7982",
+  	muertos:"79",
+  	recuperados:"58",
+  	descartados:"2526",
+  	sospechosos:"3423",
+  	pruebas:"8251",
   	aislamiento:"1805",
   	estables:"206",
   	reservado:"100",
 
-  	guayas:"1563",
-  	pichincha:"211",
-  	rios:"79",
-  	azuay:"77",
+  	guayas:"1615",
+  	pichincha:"219",
+  	rios:"80",
+  	azuay:"78",
   	manabí:"54",
   	loja:"22",
   	chimborazo:"17",
@@ -68,7 +52,5 @@ router.get('/', function(req, res, next) {
   	 });
 });
 
-router.get('/datos',roots.datos);
-router.get('/datos',roots.datos);
 
 module.exports = router;
