49792dcefbaf64551e543ccca320bcf0f2ea1682
Thor-369 <juan.p.torres@unl.edu.ec>
2021-12-08 18:33:35 -0500

    suma ok

--- public/assets/js/analisis.js
+++ public/assets/js/analisis.js
@@ -1,3 +1,4 @@
+var numeros= 1234567890;
 detectarclick1 = () => {
     if(click==0){ 
       jugado=false;                      //SI NO HICE NINGUN CLICK  - click en primera puerta
@@ -16,15 +17,18 @@ detectarclick1 = () => {
     setTimeout(function(){stats.innerHTML='Seguro que quieres esa puerta? vuelve a elegir '},800);
  
     }else{                              //SI YA HICE CLICK    - click en primera puerta por segunda vez
-      if(jugado==true){location.reload();}else{
+      if(jugado==true){ 
+        sessionStorage.wins = numWin;
+        sessionStorage.loses = numLoss;
+        sessionStorage.ratio = ratioValue;
+        location.reload();}else{
     if(winner==1){ 
+      numWin=(parseInt(1)+parseInt(numWin))*1;jugado=true;
       stats.innerHTML = 'Felicidades!'
       setTimeout(function(){stats.innerHTML='Has ganado un vehículo 0Km'},800);
-      numWin=numWin+1;jugado=true;
       }else{ 
         stats.innerHTML = 'Perdiste!'
-        numLoss=numLoss+1;
-        jugado=true;
+        numLoss=(parseInt(1)+parseInt(numLoss))*1;jugado=true;
     setTimeout(function(){stats.innerHTML='Solo te ganaste una cabra xD'},800);
       }}
       var div = document.getElementById(1);
@@ -50,14 +54,18 @@ detectarclick1 = () => {
     setTimeout(function(){stats.innerHTML='Seguro que quieres esa puerta? vuelve a elegir '},800);
  
     }else{                              //SI YA HICE CLICK    - click en segunda puerta por segunda vez
-      if(jugado==true){location.reload();}else{
+      if(jugado==true){ 
+        sessionStorage.wins = numWin;
+        sessionStorage.loses = numLoss;
+        sessionStorage.ratio = ratioValue;
+        location.reload();}else{
     if(winner==2){ 
     stats.innerHTML = 'Felicidades!'
     setTimeout(function(){stats.innerHTML='Has ganado un vehículo 0Km'},800);
-    numWin=numWin+1;jugado=true;
+    numWin=(parseInt(1)+parseInt(numWin))*1;jugado=true;
       }else{ 
         stats.innerHTML = 'Perdiste!'
-        numLoss=numLoss+1;jugado=true;
+        numLoss=(parseInt(1)+parseInt(numLoss))*1;jugado=true;
     setTimeout(function(){stats.innerHTML='Solo te ganaste una cabra xD'},800);
       }}
       var div = document.getElementById(2);
@@ -84,14 +92,18 @@ detectarclick1 = () => {
     setTimeout(function(){stats.innerHTML='Seguro que quieres esa puerta? vuelve a elegir '},800);
  
     }else{                              //SI YA HICE CLICK    - click en tercera puerta por segunda vez
-      if(jugado==true){location.reload();}else{
+      if(jugado==true){ 
+        sessionStorage.wins = numWin;
+        sessionStorage.loses = numLoss;
+        sessionStorage.ratio = ratioValue;
+        location.reload();}else{
     if(winner==3){ 
     stats.innerHTML = 'Felicidades!'
     setTimeout(function(){stats.innerHTML='Has ganado un vehículo 0Km'},800);
-    numWin=numWin+1;jugado=true;
+    numWin=(parseInt(1)+parseInt(numWin))*1;jugado=true;
       }else{ 
         stats.innerHTML = 'Perdiste!'
-        numLoss=numLoss+1;jugado=true;
+        numLoss=(parseInt(1)+parseInt(numLoss))*1;jugado=true;
     setTimeout(function(){stats.innerHTML='Solo te ganaste una cabra xD'},800);
       }}
       var div = document.getElementById(3); 
