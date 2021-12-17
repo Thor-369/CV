2178febc466e8ee84c7cc4bf29e143c9a80efee8
Thor-369 <juan.p.torres@unl.edu.ec>
2021-12-16 19:21:49 -0500

    part1

--- public/assets/js/analisis2.js
+++ public/assets/js/analisis2.js
@@ -1,142 +1,95 @@
 
-
-var numeros= 1234567890;
-detectarclick1 = () => {
-    if(click==0){ 
-      jugado=false;                      //SI NO HICE NINGUN CLICK  - click en primera puerta
-      click = 1;    
-      var perdedora = 0;
-      if(winner==1){      //Puerta 1 ganadora entonces abre random entre 2 o 3
-        perdedora=getRandomInt(2, 4)
-      }else if(winner==2){//Puerta 2 ganadora entonces abre 3
-        perdedora=3;
-      } else{           //Puerta 3 ganadora entonces abre 2
-        perdedora=2;
-      } 
-      var div = document.getElementById(perdedora);
-      div.style.webkitTransform = 'rotateY(180deg)';div.style.mozTransform = 'rotateY(180deg)';div.style.msTransform = 'rotateY(180deg)';div.style.oTransform = 'rotateY(180deg)';div.style.transform = 'rotateY(180deg)';
-    stats.innerHTML = "Buena elección!"
-    setTimeout(function(){stats.innerHTML='Seguro que quieres esa puerta? vuelve a elegir '},800); 
+ mascartas= () => { 
+  var arrayx = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+  var i, j, k;
+  for (i = arrayx.length; i; i--) {
+    j = Math.floor(Math.random() * i);
+    k = arrayx[i - 1]; 
+    arrayx[i - 1] = arrayx[j];
+    arrayx[j] = k;
+  }
+  return arrayx
+} 
+generarcartas = () =>{
+  c1=mascartas();
+  c2=mascartas();
+  c3=mascartas();
+  c4=mascartas();
+  accionBaraja();
   
-    }else{                              //SI YA HICE CLICK    - click en primera puerta por segunda vez
-      if(jugado==true){ 
-        sessionStorage.wins = numWin;
-        sessionStorage.loses = numLoss;
-        sessionStorage.ratio = parseInt(numWin)/(parseInt(numLoss)+parseInt(numWin));
-        location.reload();}else{
-    if(winner==1){ 
-      numWin=(parseInt(1)+parseInt(numWin))*1;jugado=true;
-      accionBlink(); 
-      stats.innerHTML = 'Felicidades!'
-      setTimeout(function(){stats.innerHTML='Has ganado un vehículo 0Km'},800);
-      }else{ 
-        stats.innerHTML = 'Perdiste!';
-        accionMee(); 
-        numLoss=(parseInt(1)+parseInt(numLoss))*1;jugado=true;
-    setTimeout(function(){stats.innerHTML='Solo te ganaste una cabra xD'},800);
-      }}
-      var div = document.getElementById(1);
-      div.style.webkitTransform = 'rotateY(180deg)';div.style.mozTransform = 'rotateY(180deg)';div.style.msTransform = 'rotateY(180deg)';div.style.oTransform = 'rotateY(180deg)';div.style.transform = 'rotateY(180deg)';
-      var div2 = document.getElementById(2);
-      div2.style.webkitTransform = 'rotateY(180deg)';div2.style.mozTransform = 'rotateY(180deg)';div2.style.msTransform = 'rotateY(180deg)';div2.style.oTransform = 'rotateY(180deg)';div2.style.transform = 'rotateY(180deg)';
-      var div2 = document.getElementById(3);
-      div2.style.webkitTransform = 'rotateY(180deg)';div2.style.mozTransform = 'rotateY(180deg)';div2.style.msTransform = 'rotateY(180deg)';div2.style.oTransform = 'rotateY(180deg)';div2.style.transform = 'rotateY(180deg)';
-      setTimeout(function(){window.open('?','_self')}, 20000);
-    }
-  }
-
-
-
-
-  detectarclick2 = () => {
-    if(click==0){ 
-      jugado=false;                      //SI NO HICE NINGUN CLICK  - en click segunda puerta
-      click = 1;    
-      var perdedora = 0;
-      if(winner==1){      //Puerta 1 ganadora entonces abre 3
-        perdedora=3;
-      }else if(winner==2){//Puerta 2 ganadora entonces abre random 1 o 3
-        if(boolean()==true){
-        perdedora=1;}else{perdedora=3}
-      } else{           //Puerta 3 ganadora entonces abre 1
-        perdedora=1;
+  veces=veces+1;
+  document.getElementById("exe").innerHTML=veces; 
+  clase=""; 
+  var aux=0;
+  var i=0;
+  var j=0; 
+  while(j<13){ 
+      id="1"+(j+1); 
+      id2="2"+(j+1);
+      id3="3"+(j+1);
+      id4="4"+(j+1);
+      if(aux==0){ 
+        if(Number.isInteger(c1[j])==true||c1[j]=='A'){
+        document.getElementById(id).style.backgroundImage = "url('images/corazon.png')";
+        document.getElementById(id).innerHTML ="<h4 class='text-dark' style='padding-top:10px'>"+c1[j]+"</h4>"
+        }else{ 
+        document.getElementById(id).style.backgroundImage = "url('images/corazonb.png')";
+        document.getElementById(id).innerHTML ="<h4 class='text-dark' style='padding-top:10px'>"+c1[j]+"</h4>"
+        }
+        if(j==12){
+          aux=1;j=-1;
+        }
       } 
-      var div = document.getElementById(perdedora); //Le doy la vuelta a la puerta perdedora RANDOM
-      div.style.webkitTransform = 'rotateY(180deg)'; div.style.mozTransform = 'rotateY(180deg)';div.style.msTransform = 'rotateY(180deg)';div.style.oTransform = 'rotateY(180deg)'; div.style.transform = 'rotateY(180deg)'; 
-    stats.innerHTML = "Buena elección!"
-    setTimeout(function(){stats.innerHTML='Seguro que quieres esa puerta? vuelve a elegir '},800);
- 
-    }else{                              //SI YA HICE CLICK    - click en segunda puerta por segunda vez
-      if(jugado==true){ 
-        sessionStorage.wins = numWin;
-        sessionStorage.loses = numLoss;
-        sessionStorage.ratio = parseInt(numWin)/(parseInt(numLoss)+parseInt(numWin));
-        location.reload();}else{
-    if(winner==2){ 
-      accionBlink(); 
-    stats.innerHTML = 'Felicidades!'
-    setTimeout(function(){stats.innerHTML='Has ganado un vehículo 0Km'},800);
-    numWin=(parseInt(1)+parseInt(numWin))*1;jugado=true;
-      }else{ 
-        stats.innerHTML = 'Perdiste!';
-        accionMee(); 
-        numLoss=(parseInt(1)+parseInt(numLoss))*1;jugado=true;
-    setTimeout(function(){stats.innerHTML='Solo te ganaste una cabra xD'},800);
-      }}
-      var div = document.getElementById(1);
-      div.style.webkitTransform = 'rotateY(180deg)';div.style.mozTransform = 'rotateY(180deg)';div.style.msTransform = 'rotateY(180deg)';div.style.oTransform = 'rotateY(180deg)';div.style.transform = 'rotateY(180deg)';
-      var div2 = document.getElementById(2);
-      div2.style.webkitTransform = 'rotateY(180deg)';div2.style.mozTransform = 'rotateY(180deg)';div2.style.msTransform = 'rotateY(180deg)';div2.style.oTransform = 'rotateY(180deg)';div2.style.transform = 'rotateY(180deg)';
-      var div2 = document.getElementById(3);
-      div2.style.webkitTransform = 'rotateY(180deg)';div2.style.mozTransform = 'rotateY(180deg)';div2.style.msTransform = 'rotateY(180deg)';div2.style.oTransform = 'rotateY(180deg)';div2.style.transform = 'rotateY(180deg)';
-      setTimeout(function(){window.open('?','_self')}, 20000);
-    }
-  }
-
-
-
-
-  detectarclick3 = () => {
-    if(click==0){
-      jugado=false;                       //SI NO HICE NINGUN CLICK  - click en tercera puerta
-      click = 1;    
-      var perdedora = 0;
-      if(winner==1){      //Puerta 1 ganadora entonces abre 2
-        perdedora=2
-      }else if(winner==2){//Puerta 2 ganadora entonces abre 3
-        perdedora=1;
-      } else{           //Puerta 3 ganadora entonces abre 1 o 2
-        perdedora=getRandomInt(1, 3);
+      if(aux==1){ 
+        if(Number.isInteger(c2[j])==true||c2[j]=='A'){
+        document.getElementById(id2).style.backgroundImage = "url('images/negras.png')";
+        document.getElementById(id2).innerHTML ="<h4 class='text-dark' style='padding-top:10px'>"+c2[j]+"</h4>"
+        }else{ 
+        document.getElementById(id2).style.backgroundImage = "url('images/negrasb.png')";
+        document.getElementById(id2).innerHTML ="<h4 class='text-dark' style='padding-top:10px'>"+c2[j]+"</h4>"
+        }
+        if(j==12){
+          aux=2;j=-1;
+        }
       } 
-      var div = document.getElementById(perdedora);
-      div.style.webkitTransform = 'rotateY(180deg)';div.style.mozTransform = 'rotateY(180deg)';div.style.msTransform = 'rotateY(180deg)';div.style.oTransform = 'rotateY(180deg)';div.style.transform = 'rotateY(180deg)';
+      if(aux==2){ 
+        if(Number.isInteger(c3[j])==true||c3[j]=='A'){
+        document.getElementById(id3).style.backgroundImage = "url('images/diamante.png')";
+        document.getElementById(id3).innerHTML ="<h4 class='text-dark' style='padding-top:10px'>"+c3[j]+"</h4>"
+        
+        }else{ 
+        document.getElementById(id3).style.backgroundImage = "url('images/diamanteb.png')";
+        document.getElementById(id3).innerHTML ="<h4 class='text-dark' style='padding-top:10px'>"+c3[j]+"</h4>"
+        }
+        if(j==12){
+          aux=3;j=-1;
+        }
+      } 
+      if(aux==3){ 
+        if(Number.isInteger(c4[j])==true||c4[j]=='A'){
+        document.getElementById(id4).style.backgroundImage = "url('images/trebol.png')";
+        document.getElementById(id4).innerHTML ="<h4 class='text-dark' style='padding-top:10px'>"+c4[j]+"</h4>"
+        if(j==12){
+        sessionStorage.i = 0;
+        }
+        }else{ 
+        document.getElementById(id4).style.backgroundImage = "url('images/trebolb.png')";
+        document.getElementById(id4).innerHTML ="<h4 class='text-dark' style='padding-top:10px'>"+c4[j]+"</h4>"
+        }
+        if(j==12){
+          aux=4; 
+        }
+      } 
+    j=j+1;
+    } 
+} 
 
-    stats.innerHTML = "Buena elección!"
-    setTimeout(function(){stats.innerHTML='Seguro que quieres esa puerta? vuelve a elegir '},800);
- 
-    }else{                              //SI YA HICE CLICK    - click en tercera puerta por segunda vez
-      if(jugado==true){ 
-        sessionStorage.wins = numWin;
-        sessionStorage.loses = numLoss;
-        sessionStorage.ratio = parseInt(numWin)/(parseInt(numLoss)+parseInt(numWin));
-        location.reload();}else{
-    if(winner==3){ 
-      accionBlink(); 
-    stats.innerHTML = 'Felicidades!'
-    setTimeout(function(){stats.innerHTML='Has ganado un vehículo 0Km';
-  win.innerHTML(""+numWin)},800);
-    numWin=(parseInt(1)+parseInt(numWin))*1;jugado=true; 
-      }else{ 
-        stats.innerHTML = 'Perdiste!';
-        accionMee(); 
-        numLoss=(parseInt(1)+parseInt(numLoss))*1;jugado=true;
-    setTimeout(function(){stats.innerHTML='Solo te ganaste una cabra xD'},800);
-      }}
-      var div = document.getElementById(1);
-      div.style.webkitTransform = 'rotateY(180deg)';div.style.mozTransform = 'rotateY(180deg)';div.style.msTransform = 'rotateY(180deg)';div.style.oTransform = 'rotateY(180deg)';div.style.transform = 'rotateY(180deg)';
-      var div2 = document.getElementById(2);
-      div2.style.webkitTransform = 'rotateY(180deg)';div2.style.mozTransform = 'rotateY(180deg)';div2.style.msTransform = 'rotateY(180deg)';div2.style.oTransform = 'rotateY(180deg)';div2.style.transform = 'rotateY(180deg)';
-      var div2 = document.getElementById(3);
-      div2.style.webkitTransform = 'rotateY(180deg)';div2.style.mozTransform = 'rotateY(180deg)';div2.style.msTransform = 'rotateY(180deg)';div2.style.oTransform = 'rotateY(180deg)';div2.style.transform = 'rotateY(180deg)';
-      setTimeout(function(){window.open('?','_self')}, 20000); }
-  } 
+while (z < 52) {
+  if (Number.isInteger(c1[z]) == true || c1[z] == 'A') {
+    document.getElementsByName(z)[1].innerHTML = "<h4 class='text-dark' style='padding-top:10px'>" + c1[z] + "</h4>";
+  } else {
+    document.getElementsByName(z)[1].style.backgroundImage = "url('images/negrasb.png')";
+    document.getElementsByName(z)[1].innerHTML = "<h4 class='text-dark' style='padding-top:10px'>" + c1[z] + "</h4>";
+  }
+  z = z + 1;
+}
