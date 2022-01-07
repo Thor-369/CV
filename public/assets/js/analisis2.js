664aad37399d77045bb27a85cb5312f209be0630
Thor-369 <juan.p.torres@unl.edu.ec>
2022-01-06 20:55:40 -0500

    arreglandovista

--- public/assets/js/analisis2.js
+++ public/assets/js/analisis2.js
@@ -91,16 +91,59 @@ ordenar = () => {
     }
   }
 }
-
+var auxNumber="";
+var idarranque="11";
 next =()=>{
+///Funcion para las cartas 
+//document.getElementById("12").style.scale = "150%";  
+auxNumber=document.getElementById(idarranque).textContent;
+grupo1=document.getElementById("g1"); 
+t13.pop();n13.pop();  
+carte="<div class='carta'><div style='background-image: "+document.getElementById('11').style.backgroundImage+"; class='cara cardx  text-center text-white mx-auto img-fluid ' id='11' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n1[3]+" </p></div></div>";
+document.getElementById("g1").innerHTML="<div class='carta'><div style='background-image: "+document.getElementById(idarranque).style.backgroundImage+"; class='cara cardx  text-center text-white mx-auto img-fluid ' id='"+idarranque+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n1[3]+" </p></div></div>"+grupo1.innerHTML;
 
+if (auxNumber="A") { 
+  carta="<div class='carta'> <div style='background-image: "+document.getElementById(idarranque).style.backgroundImage+"; class='cara cardx  text-center text-white mx-auto img-fluid ' onclick='next()' id='"+idarranque+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n1[3]+" </p></div></div>";
+      document.getElementById("g1").innerHTML;
+      grupo1=carta+grupo1; 
+      auxNumber=document.getElementById("410").textContent;
+      idarranque="";
+}else if(auxNumber="2"){
+  
+}else if(auxNumber="3"){
+  
+}else if(auxNumber="4"){
+  
+}else if(auxNumber="5"){
+  
+}else if(auxNumber="6"){
+  
+}else if(auxNumber="7"){
+  
+}else if(auxNumber="8"){
+  
+}else if(auxNumber="9"){
+  
+}else if(auxNumber="10"){
+  
+}else if(auxNumber="J"){
+  
+}else if(auxNumber="Q"){
+  
+}else if(auxNumber="K"){
+  
+}
+var el = document.getElementById( '11' );
+el.parentNode.removeChild( el );
 }
 var html="";
 show =()=>{
-  for (let i = 0; i < 13; i++) {   
+  var idx = [11, 12, 13, 14, 15, 16, 17, 18, 19, 110, 111, 112, 113, 21, 22, 23, 24, 25, 26, 27, 28, 29, 210, 211, 212, 213, 31, 32, 33, 34, 35, 36, 37, 38, 39, 310, 311, 312, 313, 41, 42, 43, 44, 45, 46, 47, 48, 49, 410, 411, 412, 413]
+  
+  for (let i = 0; i < 13; i++) {
    if (i==0) { 
     for (let j = 0; j < 4; j++) {   
-      carta="<div class='carta'> <div style='background-image: url"+t1[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid ' id='"+id1[j]+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n1[j]+" </p></div></div>";
+      carta="<div class='carta'> <div style='background-image: url"+t1[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid '  onclick='next()'  id='"+idx.pop()+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n1[j]+" </p></div></div>";
       html=html+carta+" ";  
       document.getElementById("g"+(i+1)).innerHTML=html; 
     }  
@@ -108,14 +151,14 @@ show =()=>{
    }
    else if(i==1) {
     for (let j = 0; j < 4; j++) {  
-      carta="<div class='carta'> <div style='background-image: url"+t2[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid ' id='"+id2[j]+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n2[j]+" </p></div></div>";
+      carta="<div class='carta'> <div style='background-image: url"+t2[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid '  onclick='next()'  id='"+idx.pop()+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n2[j]+" </p></div></div>";
       html=html+carta+"</br>";  
       document.getElementById("g"+(i+1)).innerHTML=html; 
     } 
     html=""; 
      } else if(i==2) {
       for (let j = 0; j < 4; j++) {  
-        carta="<div class='carta'> <div style='background-image: url"+t3[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid ' id='"+id3[j]+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n3[j]+" </p></div></div>";
+        carta="<div class='carta'> <div style='background-image: url"+t3[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid ' id='"+idx.pop()+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n3[j]+" </p></div></div>";
         html=html+carta+"</br>";  
         document.getElementById("g"+(i+1)).innerHTML=html; 
       } 
@@ -123,7 +166,7 @@ show =()=>{
     html=""; 
      } else if(i==3) {
       for (let j = 0; j < 4; j++) {  
-        carta="<div class='carta'> <div style='background-image: url"+t4[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid ' id='"+id4[j]+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n4[j]+" </p></div></div>";
+        carta="<div class='carta'> <div style='background-image: url"+t4[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid ' id='"+idx.pop()+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n4[j]+" </p></div></div>";
         html=html+carta+"</br>";  
         document.getElementById("g"+(i+1)).innerHTML=html; 
       } 
@@ -131,7 +174,7 @@ show =()=>{
     html=""; 
      } else if(i==4) {
       for (let j = 0; j < 4; j++) {  
-        carta="<div class='carta'> <div style='background-image: url"+t5[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid ' id='"+id5[j]+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n5[j]+" </p></div></div>";
+        carta="<div class='carta'> <div style='background-image: url"+t5[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid ' id='"+idx.pop()+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n5[j]+" </p></div></div>";
         html=html+carta+"</br>";  
         document.getElementById("g"+(i+1)).innerHTML=html; 
       } 
@@ -139,7 +182,7 @@ show =()=>{
     html=""; 
      } else if(i==5) {
       for (let j = 0; j < 4; j++) {  
-        carta="<div class='carta'> <div style='background-image: url"+t6[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid ' id='"+id6[j]+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n6[j]+" </p></div></div>";
+        carta="<div class='carta'> <div style='background-image: url"+t6[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid ' id='"+idx.pop()+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n6[j]+" </p></div></div>";
         html=html+carta+"</br>";  
         document.getElementById("g"+(i+1)).innerHTML=html; 
       } 
@@ -148,7 +191,7 @@ show =()=>{
     html=""; 
      } else if(i==6) {
       for (let j = 0; j < 4; j++) {  
-        carta="<div class='carta'> <div style='background-image: url"+t7[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid ' id='"+id7[j]+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n7[j]+" </p></div></div>";
+        carta="<div class='carta'> <div style='background-image: url"+t7[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid ' id='"+idx.pop()+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n7[j]+" </p></div></div>";
         html=html+carta+"</br>";  
         document.getElementById("g"+(i+1)).innerHTML=html; 
       } 
@@ -157,7 +200,7 @@ show =()=>{
     html=""; 
      } else if(i==7) {
       for (let j = 0; j < 4; j++) {  
-        carta="<div class='carta'> <div style='background-image: url"+t8[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid ' id='"+id8[j]+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n8[j]+" </p></div></div>";
+        carta="<div class='carta'> <div style='background-image: url"+t8[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid ' id='"+idx.pop()+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n8[j]+" </p></div></div>";
         html=html+carta+"</br>";  
         document.getElementById("g"+(i+1)).innerHTML=html; 
       } 
@@ -166,7 +209,7 @@ show =()=>{
     html=""; 
      } else if(i==8) {
       for (let j = 0; j < 4; j++) {  
-        carta="<div class='carta'> <div style='background-image: url"+t9[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid ' id='"+id9[j]+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n9[j]+" </p></div></div>";
+        carta="<div class='carta'> <div style='background-image: url"+t9[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid ' id='"+idx.pop()+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n9[j]+" </p></div></div>";
         html=html+carta+"</br>";  
         document.getElementById("g"+(i+1)).innerHTML=html; 
       } 
@@ -175,7 +218,7 @@ show =()=>{
     html=""; 
      } else if(i==9) {
       for (let j = 0; j < 4; j++) {  
-        carta="<div class='carta'> <div style='background-image: url"+t10[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid ' id='"+id10[j]+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n10[j]+" </p></div></div>";
+        carta="<div class='carta'> <div style='background-image: url"+t10[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid ' id='"+idx.pop()+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n10[j]+" </p></div></div>";
         html=html+carta+"</br>";  
         document.getElementById("g"+(i+1)).innerHTML=html; 
       } 
@@ -184,7 +227,7 @@ show =()=>{
     html=""; 
      } else if(i==10) {
       for (let j = 0; j < 4; j++) {  
-        carta="<div class='carta'> <div style='background-image: url"+t11[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid ' id='"+id11[j]+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n11[j]+" </p></div></div>";
+        carta="<div class='carta'> <div style='background-image: url"+t11[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid ' id='"+idx.pop()+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n11[j]+" </p></div></div>";
         html=html+carta+"</br>";  
         document.getElementById("g"+(i+1)).innerHTML=html; 
       } 
@@ -193,7 +236,7 @@ show =()=>{
     html=""; 
      } else if(i==11) {
       for (let j = 0; j < 4; j++) {  
-        carta="<div class='carta'> <div style='background-image: url"+t12[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid ' id='"+id12[j]+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n12[j]+" </p></div></div>";
+        carta="<div class='carta'> <div style='background-image: url"+t12[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid ' id='"+idx.pop()+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n12[j]+" </p></div></div>";
         html=html+carta+"</br>";  
         document.getElementById("g"+(i+1)).innerHTML=html; 
       } 
@@ -202,19 +245,16 @@ show =()=>{
         
      } else if(i==12) {
       for (let j = 0; j < 4; j++) {  
-        carta="<div class='carta'> <div style='background-image: url"+t13[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid ' id='"+id13[j]+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n13[j]+" </p></div></div>";
+        carta="<div class='carta'> <div style='background-image: url"+t13[j]+"' class='cara cardx  text-center text-white mx-auto img-fluid ' onclick='next()'  id='"+idx.pop()+"' name='1'><p class='text-dark' style='padding-top: 30px;padding-bottom: 30px;'>"+n13[j]+" </p></div></div>";
         html=html+carta+"</br>";  
         document.getElementById("g"+(i+1)).innerHTML=html; 
       } 
       html=""; 
-        
-     }  
+     }
   }
   document.getElementById("dialogo").innerHTML="Ahora, selecciona la carta de en medio para continuar"; 
    n1 = [], n2 = [], n3 = [], n4 = [], n5 = [], n6 = [], n7 = [], n8 = [], n9 = [], n10 = [], n11 = [], n12 = [], n13 = [];
    t1 = [], t2 = [], t3 = [], t4 = [], t5 = [], t6 = [], t7 = [], t8 = [], t9 = [], t10 = [], t11 = [], t12 = [], t13 = [];
-   id1 = [], id2 = [], id3 = [], id4 = [], id5 = [], id6 = [], id7 = [], id8 = [], id9 = [], id10 = [], id11 = [], id12 = [], id13 = [];
-  
 }
 
 
@@ -223,15 +263,12 @@ var auxTp = "";
 var auxNum = 0;
 var n1 = [], n2 = [], n3 = [], n4 = [], n5 = [], n6 = [], n7 = [], n8 = [], n9 = [], n10 = [], n11 = [], n12 = [], n13 = [];
 var t1 = [], t2 = [], t3 = [], t4 = [], t5 = [], t6 = [], t7 = [], t8 = [], t9 = [], t10 = [], t11 = [], t12 = [], t13 = [];
-var id1 = [], id2 = [], id3 = [], id4 = [], id5 = [], id6 = [], id7 = [], id8 = [], id9 = [], id10 = [], id11 = [], id12 = [], id13 = [];
-var ultimaCarta=""; 
 var exe=0;
 find = () => { 
   exe=exe+1;
   document.getElementById("exe").innerHTML=exe;
   nums=["A","1","2","3","4","5","6","7","8","9","10"];
   var ids = [11, 12, 13, 14, 15, 16, 17, 18, 19, 110, 111, 112, 113, 21, 22, 23, 24, 25, 26, 27, 28, 29, 210, 211, 212, 213, 31, 32, 33, 34, 35, 36, 37, 38, 39, 310, 311, 312, 313, 41, 42, 43, 44, 45, 46, 47, 48, 49, 410, 411, 412, 413]
-  var id = shufflx(ids); 
   var arraxx = [{ "numero": "A", "tipo": "1", }, { "numero": "2", "tipo": "1", }, { "numero": "3", "tipo": "1", }, { "numero": "4", "tipo": "1", }, { "numero": "5", "tipo": "1", }, { "numero": "6", "tipo": "1", }, { "numero": "7", "tipo": "1", }, { "numero": "8", "tipo": "1", }, { "numero": "9", "tipo": "1", }, { "numero": "10", "tipo": "1", }, { "numero": "J", "tipo": "1", }, { "numero": "Q", "tipo": "1", }, { "numero": "K", "tipo": "1", }, { "numero": "A", "tipo": "2", }, { "numero": "2", "tipo": "2", }, { "numero": "3", "tipo": "2", }, { "numero": "4", "tipo": "2", }, { "numero": "5", "tipo": "2", }, { "numero": "6", "tipo": "2", }, { "numero": "7", "tipo": "2", }, { "numero": "8", "tipo": "2", }, { "numero": "9", "tipo": "2", }, { "numero": "10", "tipo": "2", }, { "numero": "J", "tipo": "2", }, { "numero": "Q", "tipo": "2", }, { "numero": "K", "tipo": "2", }, { "numero": "A", "tipo": "3", }, { "numero": "2", "tipo": "3", }, { "numero": "3", "tipo": "3", }, { "numero": "4", "tipo": "3", }, { "numero": "5", "tipo": "3", }, { "numero": "6", "tipo": "3", }, { "numero": "7", "tipo": "3", }, { "numero": "8", "tipo": "3", }, { "numero": "9", "tipo": "3", }, { "numero": "10", "tipo": "3", }, { "numero": "J", "tipo": "3", }, { "numero": "Q", "tipo": "3", }, { "numero": "K", "tipo": "3", }, { "numero": "A", "tipo": "4", }, { "numero": "2", "tipo": "4", }, { "numero": "3", "tipo": "4", }, { "numero": "4", "tipo": "4", }, { "numero": "5", "tipo": "4", }, { "numero": "6", "tipo": "4", }, { "numero": "7", "tipo": "4", }, { "numero": "8", "tipo": "4", }, { "numero": "9", "tipo": "4", }, { "numero": "10", "tipo": "4", }, { "numero": "J", "tipo": "4", }, { "numero": "Q", "tipo": "4", }, { "numero": "K", "tipo": "4", },]
   var arrax = shufflx(arraxx); 
   var auxid ="";
@@ -240,7 +277,6 @@ find = () => {
 
   for (let q = 0; q < 52; q++) {  
     //auxid Tomando el id "chocoleado-shufle"; auxTp tomando el tipo "Color"; auxNum Tomando el numero  
-   auxid =id[q]; 
    auxNum = arrax[q].numero;  
     if(nums.includes(`${arrax[q].numero}`)==true){ //Si es número: esto para mostrar la imagen para nùmeros
       if(`${arrax[q].tipo}`=="1"){ //Si es Corazón rojo
@@ -265,74 +301,60 @@ find = () => {
       }
       }  
     if (between(q, 0, 3)) { 
-      id1.push(auxid);  
       n1.push(auxNum); 
       t1.push(auxTp);  
 
     }  if (between(q, 4, 7)) {
-      id2.push(auxid);  
       n2.push(auxNum); 
       t2.push(auxTp);  
 
     } if (between(q, 8, 11)) {
-      id3.push(auxid);  
       n3.push(auxNum); 
       t3.push(auxTp);  
       
     } if (between(q, 12, 15)) {
-      id4.push(auxid);  
       n4.push(auxNum); 
       t4.push(auxTp);  
       
     } if (between(q, 16,19 )) {
-      id5.push(auxid);  
       n5.push(auxNum); 
       t5.push(auxTp);  
       
     } if (between(q, 20, 23)) {
-      id6.push(auxid);  
       n6.push(auxNum); 
       t6.push(auxTp);  
       
     } if (between(q, 24, 27)) {
-      id7.push(auxid);  
       n7.push(auxNum); 
       t7.push(auxTp);  
       
     } if (between(q, 28, 31)) {
-      id8.push(auxid);  
       n8.push(auxNum); 
       t8.push(auxTp);  
       
     } if (between(q, 32, 35)) {
-      id9.push(auxid);  
       n9.push(auxNum); 
       t9.push(auxTp);  
       
     } if (between(q, 36, 39)) {
-      id10.push(auxid);  
       n10.push(auxNum); 
       t10.push(auxTp);  
       
     } if (between(q, 40, 43)) {
-      id11.push(auxid);  
       n11.push(auxNum); 
       t11.push(auxTp);  
       
     } if (between(q, 44, 47)) {
-      id12.push(auxid);  
       n12.push(auxNum); 
       t12.push(auxTp);  
       
     } if (between(q, 48, 51)) {
-      id13.push(auxid);  
       n13.push(auxNum); 
       t13.push(auxTp);   
     } 
 
   } 
-  
-  ultimaCarta=id13[3];
+   
   show();
   //Hacer funcion para poner aqui que se ejecute luego de 0 segundos y muestre todas las cartas 
   // auxsegundos=aumento en 1 por cada posibilidad, si estancamiento, no aumento, muere, fin xd 
